2f1553473380ac915a8587c1b74c09a304165d08
Hristijan <hristijan93@hotmail.com>
2022-11-08 12:53:57 +0100

    String ladder 8

--- main.js
+++ main.js
@@ -130,6 +130,14 @@ console.log(upArray([4,3,2,5]))
 // String ladder
 
 // 8
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+function areYouPlayingBanjo(name) {
+  return name[0] == `r` || name[0] == `R` ? name + " plays banjo" : name + " does not play banjo"
+}
+
+console.log(areYouPlayingBanjo(`Hristijan`))
 
 // 7
 
