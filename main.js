796812f852c2103ae68e6d725266a389a1c58d17
Hristijan <hristijan93@hotmail.com>
2022-11-10 10:48:01 +0100

    FizzBuzz

--- main.js
+++ main.js
@@ -201,9 +201,41 @@ console.log(upArray([4,3,2,5]))
 
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
-function removeExclamationMarks(s) {
-  return s.replace(/!/g, "");
+// function removeExclamationMarks(s) {
+//   return s.replace(/!/g, "");
+// }
+
+// console.log(removeExclamationMarks(`Hello W!!orld!`))
+
+
+
+
+// FizzBuzz
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+function fizzbuzz(n){
+  let resultArr = [];
+
+  for (let i = 1; i <= n; i++) {
+      if (i % 15 == 0) {
+          resultArr.push(`FizzBuzz`)
+      } else if (i % 5 == 0) {
+          resultArr.push(`Buzz`)
+      } else if (i % 3 ==0) {
+          resultArr.push(`Fizz`)
+      } else {
+          resultArr.push(i)
+      }        
+  }
+  
+  return resultArr;
 }
 
-console.log(removeExclamationMarks(`Hello W!!orld!`))
-
+console.log(fizzbuzz(15))
