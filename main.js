1b5d45e63c391ad6b63977998189c7dce0240bf8
Hristijan <hristijan93@hotmail.com>
2022-11-15 16:30:25 -0500

    strange principal

--- main.js
+++ main.js
@@ -300,7 +300,7 @@ console.log(vowelOne(`abceios`))
 
 
 */
-
+/*
 // Categorize New Member
 
 
@@ -332,3 +332,34 @@ function openOrSenior(data){
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 
 console.log(openOrSenior(input))
+*/
+
+// Strange Principal
+
+// A high school has a strange principal. On the first day, he has his students perform an odd opening day ceremony:
+
+// There are number of lockers "n" and number of students "n" in the school. The principal asks the first student to go to every locker and open it. Then he has the second student go to every second locker and close it. The third goes to every third locker and, if it is closed, he opens it, and if it is open, he closes it. The fourth student does this to every fourth locker, and so on. After the process is completed with the "n"th student, how many lockers are open?
+
+// The task is to write a function which gets any number as an input and returns the number of open lockers after last sudent complets his activity. So input of the function is just one number which shows number of lockers and number of students. For example if there are 1000 lockers and 1000 students in school then input is 1000 and function returns number of open lockers after 1000th student completes his action.
+
+// The given input is always an integer greater than or equal to zero that is why there is no need for validation.
+
+
+function numOfOpenLockers(n){
+  if (n == 0) {
+    return 0;
+  }
+  
+  let i = 1;
+  let sum = 0;
+  
+  while ((i * i) <= n) {
+    i++;
+    sum++;
+  }
+  
+  return sum;
+}
+
+
+console.log(numOfOpenLockers(1000))
