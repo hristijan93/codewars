b39009543c661dbfa2190b2590d5fe19857342e4
Hristijan <hristijan93@hotmail.com>
2022-11-18 16:58:30 -0500

    Incrementer task completed

--- main.js
+++ main.js
@@ -394,3 +394,15 @@ console.log(vaporcode(`Hello my name is Hristijan`))
 // Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
 
 // Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+
+function incrementer(nums) { 
+  return nums.map((el, i) => {
+      if (el + i + 1 <= 9) {
+          return el + i + 1
+      } else {
+          return +String(el + i + 1)[String(el).length]
+      }
+  })
+}
+
+console.log(incrementer([1, 2, 3, 4, 5, 6, 7]))
