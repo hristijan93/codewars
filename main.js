5d9aabbecf10c00514f33b789d9778519286e100
Hristijan <hristijan93@hotmail.com>
2022-11-24 23:17:54 +0100

    leap years

--- main.js
+++ main.js
@@ -471,7 +471,7 @@ function filter_list(l) {
 
 console.log(filter_list([1,2,'a','b']))
 */
-
+/*
 // Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
 // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
@@ -486,3 +486,4 @@ String.prototype.toJadenCase = function () {
 let str = `How can mirrors be real if our eyes aren't real`
 
 console.log(str.toJadenCase())
+*/
