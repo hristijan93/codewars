43995bacdcec5ec6eef27b9d6c8cc6ed556c3cb1
Hristijan <hristijan93@hotmail.com>
2022-11-29 23:33:54 +0100

    counts how many arguments are in a function

---
+++ functionArgumentsCount.js
@@ -0,0 +1,10 @@
+/*
+// Create a function called args_count that returns the number of arguments provided
+
+
+function args_count() {
+    return arguments.length;
+}
+
+console.log(args_count(1, 2, 3))
+*/
