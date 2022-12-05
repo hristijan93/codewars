918dcc4173e7e0ef538f0de4619e7078c913668e
Hristijan <hristijan93@hotmail.com>
2022-12-05 23:44:08 +0100

    refactored // will find the only unique number in array of repetitive numbers

--- findTheStrayNumber.js
+++ findTheStrayNumber.js
@@ -7,7 +7,7 @@
 // Examples
 // [1, 1, 2] ==> 2
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
-
+/*
 function stray(numbers) {
     let dict = {};
     
@@ -28,3 +28,19 @@ function stray(numbers) {
 
 console.log(stray([1, 1, 2]));
 console.log(stray([17, 17, 3, 17, 17, 17, 17]))
+*/
+
+// Refactor
+
+function stray(numbers) {
+    let sorted = numbers.sort();
+
+    if (sorted[0] != sorted[1]) {
+        return sorted[0]
+    } else {
+        return sorted[sorted.length - 1]
+    }
+}
+
+console.log(stray([1, 1, -19, 1, 1]));
+console.log(stray([-5, -5, -5, 505, -5, -5]));
