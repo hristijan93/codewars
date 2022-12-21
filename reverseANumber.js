55ffe93434fbdd2733b68157ffb9968a1e58ac39
hristijan93 <hristijan93@hotmail.com>
2022-12-21 23:24:27 +0100

    Reverse any number positive or negative

---
+++ reverseANumber.js
@@ -0,0 +1,18 @@
+/*
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+    let reversed = +String(Math.abs(n)).split(``).reverse().join(``)
+    return n < 0 ? -reversed : reversed
+}
+
+console.log(reverseNumber(123));
+console.log(reverseNumber(-456));
+*/
